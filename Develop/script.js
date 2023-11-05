7840c7e600c243874361a7e38a57f4a419da5c10
David Moore <dtm819@gmail.com>
2023-11-05 14:32:33 -0500

    added completed project

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,41 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  let characters = "";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let number = "0123456789";
+  let symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+  let passlength = window.prompt("Enter a number between 8 and 128 to decide password length");
+  let upper = window.prompt("Enter Y to add uppercase characters. Anything else = exclude.");
+  let lower = window.prompt("Enter Y to add lowercase characters. Anything else = exclude.");
+  let numeric = window.prompt("Enter Y to add numbers. Anything else = exclude.");
+  let special = window.prompt("Enter Y to add special characters. Anything else = exclude.");
+
+  if (upper === "Y") {
+    characters += upperCase;
+  } 
+  if (lower === "Y") {
+    characters += lowerCase;
+  }
+  if (numeric === "Y") {
+    characters += number;
+  }
+  if (special === "Y") {
+    characters += symbols;
+  }
+
+  let password = "";
+
+  for (let i = 1; i <= passlength; i++) {
+    // let randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(Math.floor(Math.random() * i));
+  }
+  document.getElementById("password").value = password;
 }
 
 // Add event listener to generate button
